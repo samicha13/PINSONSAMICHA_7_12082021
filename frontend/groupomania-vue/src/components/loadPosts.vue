54166d2ebf5fa11215a51d69320595d9c1e26eae
Samicha Pinson <samichapinson@gmail.com>
2021-09-02 23:07:14 +0300

    mise en place des posts

--- frontend/groupomania-vue/src/components/loadPosts.vue
+++ frontend/groupomania-vue/src/components/loadPosts.vue
@@ -1,16 +1,19 @@
 <template>
   <div >
-      <div id="message-card" v-for="post in Posts" :key="post.id">
-        <h1 class="title">{{ post.title }}</h1>
+      <div id="message-card" v-for="post in posts" :key="post.id">
+      
+        <h1 class="title">{{ post.titre }}</h1>
         <div class="content">
           <img class="imgLoad" :src="post.image" :alt="post.image"
             v-if="post.image != null"/> 
         </div>
-        <h1>{{ post.content }}</h1>
+           
+        <h1>{{ post.message }}</h1>
         <div class="createdAt">
           <i>{{ moment(post.createdAt).fromNow() }}</i>
         </div>
-        {{ post.User.nom }}
+         
+          
       
         <div class="adminDelete" v-if="isAdmin == true">
           <deletePost :id="post.id" />
@@ -35,7 +38,8 @@ export default {
       token: "",
       userId: localStorage.getItem("id"),
       isAdmin: "",
-      Posts: [],
+      posts: [],
+      //users: [],
       idUserp: "",
       title: "",
       content: "",
@@ -46,18 +50,38 @@ export default {
     loadForum() {
       let token = this.$store.state.user.token;
       
-      axios
-        .get("http://localhost:3000/api/posts/", {
+      axios.get("http://localhost:3000/api/posts/", {
           headers: { Authorization: "Bearer " + token },
         })
+        .then(res => {this.posts =res.data;
+        console.log(this.posts)
+         
+          })
       
         .catch((error) => {
           { error }
         });
     },
+
+    loadUser(){
+      let token = this.$store.state.user.token;
+       axios.get("http://localhost:3000/api/auth/", {
+          headers: { Authorization: "Bearer " + token },
+        })
+         .then(res => {
+        this.users = res.data;
+        console.log(this.users)
+       ;
+        
+      })
+       .catch((error) => {
+          { error }
+        });
+    }
   },
   mounted() {
     this.loadForum();
+     this.loadUser();
   },
 };
 </script>
